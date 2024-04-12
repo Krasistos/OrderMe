8f2c814a94e91f04d079317cdf7e1e807bf9548a
Krasi Kolev <krasikolev006@gmail.com>
2024-04-12 21:31:31 +0300

    All DbSets added no configurations and delete behavior managed

--- OrderMe.Infrastructure/Data/ApplicationDbContext.cs
+++ OrderMe.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OrderMe.Infrastructure.Data.Models;
 
 namespace OrderMe.Infrastructure.Data
 {
@@ -9,5 +10,23 @@ namespace OrderMe.Infrastructure.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // builder.ApplyConfiguration for everything should be here
+          
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Driver> Drivers { get; set; } = null!;
+        public DbSet<Vehicle> Vehicles { get; set; } = null!;
+        public DbSet<Garage> Garages { get; set; } = null!;
+        public DbSet<Cart> Carts { get; set; } = null!;
+        public DbSet<Ride> Rides { get; set; } = null!;
+        public DbSet<Restaurant> Restaurants { get; set; } = null!;
+        public DbSet<MenuItem> MenuItems { get; set; } = null!;
+
+        
     }
 }
